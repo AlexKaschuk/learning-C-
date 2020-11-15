8feef4906a27e5ee4d60c7c062a65f74d09d6a14
Alexander Kaschuk <58186279+AlexKaschuk@users.noreply.github.com>
2020-11-15 14:49:18 +0200

    Create Program.cs

---
+++ Adapter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int item in SequenceGenerator.GenerateRandomSequence<int>(10, -50, 100, new RandomAdapter())) {
+                Console.Write($"{item} ");
+            };
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
