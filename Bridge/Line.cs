cf30d739a8d377064f0f0e5dfbeeb87cb0288b2e
AlexKashchuk <ky3he4ik@gmail.com>
2020-11-15 15:12:31 +0200

    Update Line.cs

--- Bridge/Line.cs
+++ Bridge/Line.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,3 +19,25 @@ namespace Bridge
         
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Bridge
+{
+    public sealed class Line: IDrawable
+    {
+        public ILineImplementationable implement { get; set; }
+        public Point Start { get; set; }
+        public Point End { get; set; }
+
+        public void Draw() =>
+            implement.Plot2D(Start, End);
+        
+    }
+}
+>>>>>>> 34a0ba9b576be165f92221286a65aec0c038d6ca
