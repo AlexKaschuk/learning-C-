c5c4d2f2fb68b93f4f710629803b77c3ca8dbe32
AlexKashchuk <ky3he4ik@gmail.com>
2020-11-15 15:12:44 +0200

    Update SolidLineImplement.cs

--- Bridge/SolidLineImplement.cs
+++ Bridge/SolidLineImplement.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -14,3 +15,21 @@ namespace Bridge
         
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public class SolidLineImplement : ILineImplementationable
+    {
+        public void Plot2D(Point start, Point end)=>
+             Console.WriteLine($"solid line from {start} to {end}");
+        
+    }
+}
+>>>>>>> 34a0ba9b576be165f92221286a65aec0c038d6ca
