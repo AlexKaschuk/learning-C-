5af3cfd2847c3b495d16aea3c966b6829ec12a2b
AlexKashchuk <ky3he4ik@gmail.com>
2020-11-15 15:12:39 +0200

    Update Program.cs

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -18,3 +19,25 @@ namespace Bridge
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Line l = new Line { Start = new Point(10, 20), End = new Point(30, 40), implement = new SolidLineImplement() };
+
+            l.Draw();
+        }
+    }
+}
+>>>>>>> 34a0ba9b576be165f92221286a65aec0c038d6ca
