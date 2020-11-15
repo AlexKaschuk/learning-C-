8581efc60794fa330de63e78c405cddb6bc65cff
Alexander Kaschuk <58186279+AlexKaschuk@users.noreply.github.com>
2020-11-15 14:49:21 +0200

    Create RandomAdapter.cs

---
+++ Adapter/RandomAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class RandomAdapter :  IRandomble<int>
+    {
+        //public int RandomValue(int from, int to) => base.Next(from, to);
+
+        Random r = new Random();
+
+        public int RandomValue(int from, int to) => r.Next(from, to);
+    }
+}
