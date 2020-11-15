69781a33cc0309c8b066720875dde0e453a75f90
AlexKashchuk <ky3he4ik@gmail.com>
2020-11-15 15:12:33 +0200

    Update LineImplementationable.cs

--- Bridge/LineImplementationable.cs
+++ Bridge/LineImplementationable.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -12,3 +13,19 @@ namespace Bridge
         void Plot2D(Point start, Point end);
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public interface ILineImplementationable
+    {
+        void Plot2D(Point start, Point end);
+    }
+}
+>>>>>>> 34a0ba9b576be165f92221286a65aec0c038d6ca
