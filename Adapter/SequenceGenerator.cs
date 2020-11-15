f742ba49cd3a3be93f1f7fe3ce997676a3d51add
Alexander Kaschuk <58186279+AlexKaschuk@users.noreply.github.com>
2020-11-15 14:49:27 +0200

    Create SequenceGenerator.cs

---
+++ Adapter/SequenceGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public static class SequenceGenerator
+    {
+        public static IEnumerable<T> GenerateRandomSequence<T>(int length, T from, T to, IRandomble<T> gen) {
+            List <T> list = new List<T>();
+            for (int i = 0; i < length; ++i) {
+                list.Add(gen.RandomValue(from, to));
+            }
+
+            return list;
+        } 
+
+
+
+
+    }
+}
