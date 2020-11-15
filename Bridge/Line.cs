34a0ba9b576be165f92221286a65aec0c038d6ca
Alexander Kaschuk <58186279+AlexKaschuk@users.noreply.github.com>
2020-11-15 15:01:53 +0200

    Add files via upload

---
+++ Bridge/Line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Bridge
+{
+    public sealed class Line: IDrawable
+    {
+        public ILineImplementationable implement { get; set; }
+        public Point Start { get; set; }
+        public Point End { get; set; }
+
+        public void Draw() =>
+            implement.Plot2D(Start, End);
+        
+    }
+}
---
+++ Bridge/SolidLineImplement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public class SolidLineImplement : ILineImplementationable
+    {
+        public void Plot2D(Point start, Point end)=>
+             Console.WriteLine($"solid line from {start} to {end}");
+        
+    }
+}
